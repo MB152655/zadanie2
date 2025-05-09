1e70603ca214789a66b6ffde6df3abaa187699c1
MB152655 <szx87121@student.szczecin.merito.pl>
2025-05-09 21:19:36 +0200

    Create test_app.py

---
+++ zadanie_2/test_app.py
@@ -0,0 +1,22 @@
+import re
+from datetime import datetime
+
+def is_valid_email(email):
+    # TODO: implementacja po testach
+    pass
+
+def calculate_area_rectangle(width, height):
+    # TODO: implementacja po testach
+    pass
+
+def filter_positive_numbers(data):
+    # TODO: implementacja po testach
+    pass
+
+def convert_date_format(date_str):
+    # TODO: implementacja po testach
+    pass
+
+def is_palindrome(text):
+    # TODO: implementacja po testach
+    pass
